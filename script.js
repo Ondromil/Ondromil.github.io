f592021bd344eee2e2e41d21aa7b8dcfed52da9f
Ondřej <ondrahlavac10@gmail.com>
2022-06-10 13:30:13 +0200

    added matrix

---
+++ script.js
@@ -0,0 +1,36 @@
+let num;
+let symbols = 0;
+let eightSymbol = 0;
+let lines = 0;
+
+function display()
+{
+    num = Math.floor(Math.random() * 2);
+    document.getElementById("demo").innerHTML += num + "";
+    symbols += 1;
+    eightSymbol += 1;
+    if (eightSymbol > 7)
+    {
+        document.getElementById("demo").innerHTML += "-"
+        eightSymbol = 0;
+    }
+    if (symbols > 78)
+    {
+        document.getElementById("demo").innerHTML += "<br>";
+        symbols = 0;
+        lines += 1;
+        if (lines > 16)
+        {
+            clearInterval(interval);
+        }
+    }
+}
+
+interval = setInterval(display, 50);
+
+
+
+
+
+
+
